66da76e365fa1cc96b9d41a5534959f97fb64706
Mariabayonae <bayonam346@gmail.com>
2026-04-10 21:21:17 +0000

    quitar videos

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // Crear las listas de imágenes, títulos y frases
-const imagenes = [
-  "https://www.postposmo.com/wp-content/uploads/2020/09/Especies-end%C3%A9micas-de-colombia-3.jpg",
-  "https://www.postposmo.com/wp-content/uploads/2020/09/Especies-end%C3%A9micas-de-colombia-4.jpg",
-];
+const videos = ["
+"];
 
 const titulos = [
   "Tortuga morrocoy",
